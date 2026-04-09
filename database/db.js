be27b6041e7d02bd19b7bb02445ac985b8c94747
Thahmil Mahesh <thahmilmaheshkumar@gmail.com>
2026-04-09 10:13:34 +0530

    second

--- database/db.js
+++ database/db.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
+import dns from "dns";
+
+dns.setDefaultResultOrder("ipv4first");
 
 export const connectDB = async () => {
-  mongoose
-    .connect(process.env.MONGO_URI)
+  await mongoose
+    .connect(
+      "mongodb://root:root@ac-srpxarn-shard-00-00.oklol3n.mongodb.net:27017,ac-srpxarn-shard-00-01.oklol3n.mongodb.net:27017,ac-srpxarn-shard-00-02.oklol3n.mongodb.net:27017/?ssl=true&replicaSet=atlas-2z2np8-shard-0&authSource=admin&appName=Cluster0/note",
+    )
     .then((data) => {
       console.log(`MongoDB connected with server: ${data.connection.host}`);
     })
     .catch((err) => {
-      console.log(err.message);
+      console.log(err);
     });
 };
