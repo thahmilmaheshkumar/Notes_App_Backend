b1a8f4c8dba0e1e82b2c542c628cf77eaf1ffaef
Thahmil Mahesh <thahmilmaheshkumar@gmail.com>
2026-04-10 17:51:58 +0530

    await

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,16 +3,16 @@ import errorhandler from "../helper/errorhandler.js";
 
 export const auth = async (req, resizeBy, next) => {
   try {
-    const token = req.cookies.token;
+    const token = await req.cookies.token;
 
     if (!token) {
-      return resizeBy
+      return res
         .status(401)
         .json({ success: false, message: "Please login to access" });
     }
 
-    const decode = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decode.id;
+    const decode = await jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await decode.id;
     next();
   } catch (error) {
     return res
