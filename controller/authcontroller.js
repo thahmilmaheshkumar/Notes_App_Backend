a41eab8d195035f578416171ad69f125027385b3
Thahmil Mahesh <thahmilmaheshkumar@gmail.com>
2026-04-10 18:34:07 +0530

    secure

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -68,6 +68,7 @@ export const Login = async (req, res) => {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     sameSite: "none",
+    secure: true,
   });
 
   res.status(200).json({ success: true, message: "Login successful", token });
